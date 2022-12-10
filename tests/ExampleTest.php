e5624932191a2eb3086a33d7ed8781a294c64999
Evgenii Nasyrov <enasyrov@icloud.com>
2022-12-10 13:00:36 +0000

    Install and configure phpunit

---
+++ tests/ExampleTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Wpfortress\Sdk\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class ExampleTest extends TestCase
+{
+    /** @test */
+    public function it_passes(): void
+    {
+        $this->assertTrue(true);
+    }
+}
